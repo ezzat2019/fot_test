d9cdecc6651da49a90d105987099b5c46e6806a1
ezzat <coezz@yahoo.com>
2021-12-17 23:10:38 +0200

    rating assets

--- lib/data/add/product_model.dart
+++ lib/data/add/product_model.dart
@@ -1,28 +1,34 @@
-/// name : ""
-/// price : 10.2
-/// img_url : ""
+/// name : "shop"
+/// rates : {"id4":9.9,"id1":5.3,"id2":3.5}
+/// price : 45.2
+/// img_url : "https://www.linde-mh.com/media/Global-Content/01-Products/07_Order-Pickers_Kommissionierer/Order_picker-stacking-warehouse_16x9w1920.jpg"
 
 class ProductModel {
   ProductModel({
       String name, 
-      dynamic price,
+
+      double price, 
       String imgUrl,}){
     _name = name;
+
     _price = price;
     _imgUrl = imgUrl;
 }
 
   ProductModel.fromJson(dynamic json) {
     _name = json['name'];
+
     _price = json['price'];
     _imgUrl = json['img_url'];
   }
   String _name;
-  dynamic _price;
+
+  double _price;
   String _imgUrl;
 
   String get name => _name;
-  dynamic get price => _price;
+
+  double get price => _price;
   String get imgUrl => _imgUrl;
 
   Map<String, dynamic> toJson() {
